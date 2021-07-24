d15512d0e99a48f4c497c1b19b75bfa5300b68ba
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-07-24 17:33:42 -0300

    Adicionando validação de id

--- script.js
+++ script.js
@@ -36,21 +36,26 @@ function cadastrar(qtdAlunos) {
 	novoAluno.nome  = document.querySelector('input#txtNome').value
 	if(verificaNome(novoAluno.nome) == 1)
 		confirmacoes++
+	else
+		window.alert(`Nome inválido.`)
 	
 	novoAluno.sexo  = document.querySelector('select#sSexo').value
 	if(verificaSexo(novoAluno.sexo) == 1)
 		confirmacoes++
+	else
+		window.alert(`Sexo inválido.`)
 
 	novoAluno.idade = document.querySelector('input#numIdade').value
 	if(verificaIdade(novoAluno.idade) == 1)
 		confirmacoes++
+	else
+		window.alert(`Idade inválida.`)
 
 	if(confirmacoes == 3) {
 		arrayAlunos.push(novoAluno)
 		this.qtdAlunos++;
 		window.alert(`Aluno ${this.qtdAlunos} cadastrado com sucesso.`)
-	} else
-		window.alert('Houve um erro em uma ou mais credenciais.')
+	}
 }
 
 function listar(qtdAlunos) {
@@ -84,39 +89,48 @@ function listar(qtdAlunos) {
 }
 
 let btnAlterarNome = document.getElementById('btnAlterarNome')
-btnAlterarNome.addEventListener('click', () => {
-	let idAluno  = document.getElementById('alterarAluno').value
+btnAlterarNome.addEventListener('click', (qtdAlunos) => {
 	let novoNome = document.getElementById('novoNome').value
-	arrayAlunos[idAluno-1].nome = novoNome
-	window.alert(`Nome do aluno ${idAluno} alterado para ${novoNome} com sucesso.`)
+	let idAluno  = document.getElementById('alterarAluno').value
+	if(verificaId(idAluno, this.qtdAlunos) == 1) {
+		arrayAlunos[idAluno-1].nome = novoNome
+		window.alert(`Nome do aluno ${idAluno} alterado para ${novoNome} com sucesso.`)
+	} else
+		window.alert(`ID inválido.`)
 })
 
 let btnAlterarSexo = document.getElementById('btnAlterarSexo')
-btnAlterarSexo.addEventListener('click', () => {
-	let idAluno  = document.getElementById('alterarAluno').value
+btnAlterarSexo.addEventListener('click', (qtdAlunos) => {
 	let novoSexo = document.getElementById('sNovoSexo').value
-	arrayAlunos[idAluno-1].sexo = novoSexo
-	window.alert(`Sexo do aluno ${idAluno} alterado para ${novoSexo} com sucesso.`)
+	let idAluno  = document.getElementById('alterarAluno').value
+	if(verificaId(idAluno, this.qtdAlunos) == 1) {
+		arrayAlunos[idAluno-1].sexo = novoSexo
+		window.alert(`Sexo do aluno ${idAluno} alterado para ${novoSexo} com sucesso.`)
+	} else	
+		window.alert(`ID inválido.`)
 })
 
 let btnAlterarIdade = document.getElementById('btnAlterarIdade')
-btnAlterarIdade.addEventListener('click', () => {
-	let idAluno   = document.getElementById('alterarAluno').value
+btnAlterarIdade.addEventListener('click', (qtdAlunos) => {
 	let novaIdade = document.getElementById('novaIdade').value
-	arrayAlunos[idAluno-1].idade = novaIdade
-	window.alert(`Idade do aluno ${idAluno} alterada para ${novaIdade} com sucesso.`)
+	let idAluno   = document.getElementById('alterarAluno').value
+	if(verificaId(idAluno, this.qtdAlunos) == 1) {
+		arrayAlunos[idAluno-1].idade = novaIdade
+		window.alert(`Idade do aluno ${idAluno} alterada para ${novaIdade} com sucesso.`)
+	} else
+		window.alert(`ID inválido.`)
 })
 
 function remover(qtdAlunos) {
 
 	if(this.qtdAlunos > 0) {
 		let idAluno = document.getElementById("idAluno").value
-
-		arrayAlunos.splice(idAluno-1, 1)
-		
-		this.qtdAlunos--;
-		
-		window.alert(`Aluno ${idAluno} removido com sucesso.`)
+		if(verificaId(idAluno, this.qtdAlunos) == 1) {
+			arrayAlunos.splice(idAluno-1, 1)
+			this.qtdAlunos--;
+			window.alert(`Aluno ${idAluno} removido com sucesso.`)
+		} else
+			window.alert(`ID inválido.`)
 	} else
 		window.alert('Nenhum aluno na lista.')
 }
@@ -142,6 +156,15 @@ function verificaSexo(sexo) {
 function verificaIdade(idade) {
 	let retorno = 0
 	if(idade <= 3 || idade >= 100)
+		retorno = 0
+	else
+		retorno = 1
+	return retorno
+}
+
+function verificaId(id, qtdAlunos) {
+	let retorno = 0
+	if(id < 1 || id > this.qtdAlunos)
 		retorno = 0
 	else
 		retorno = 1
